ad8152d9bb24eb43e7ca564d399669b5fcb38345
Riley Loudon <rileyloudon@icloud.com>
2019-09-15 21:56:43 -0500

    added support for custom canvas sizes

--- script.js
+++ script.js
@@ -1,21 +1,52 @@
 const container = document.getElementById('container');
+const colorful = document.getElementById('colorful');
+const greyscale = document.getElementById('greyscale');
+const changeSize = document.getElementById('changeSize');
 
 let rows = 16;
-let columns = 16;
-let totalArea = rows * columns;
 
-for (i = 0; i < totalArea; i++) {
-  const div = document.createElement('div');
-  div.setAttribute('class', 'squares');
-  container.appendChild(div);
+createPaintingBox(rows);
+getSquares();
+addColorBox(allSquares);
+
+function createPaintingBox(rows) {
+  let totalArea = rows * rows;
+
+  container.style.setProperty('grid-template-columns', `repeat(${rows}, auto)`);
+
+  for (i = 0; i < totalArea; i++) {
+    const div = document.createElement('div');
+    div.setAttribute('class', 'squares');
+    container.appendChild(div);
+  }
 }
 
-let squares = document.querySelectorAll('.squares');
+function getSquares() {
+  const squares = document.querySelectorAll('.squares');
+  return (allSquares = squares);
+}
 
-squares.forEach(box => {
-  box.addEventListener('mouseover', () => {
-    // Set the background to a random color:
-    box.style.backgroundColor =
-      '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+function addColorBox(allSquares) {
+  allSquares.forEach(box => {
+    box.addEventListener('mouseover', () => {
+      // Set the background to a random color:
+      box.style.backgroundColor =
+        '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
+    });
   });
+}
+
+changeSize.addEventListener('click', () => {
+  let size = prompt('How many squares across would you like?');
+  rows = size;
+  removePaintingBox();
+  createPaintingBox(rows);
+  getSquares();
+  addColorBox(allSquares);
 });
+
+function removePaintingBox() {
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+}
