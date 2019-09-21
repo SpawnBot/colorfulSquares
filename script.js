a1d260d3dbe7a5315a8febfc9f15fb1384a79f52
Riley Loudon <rileyloudon@icloud.com>
2019-09-21 16:00:30 -0500

    added greyscale functionality

--- script.js
+++ script.js
@@ -5,8 +5,12 @@ const changeSizeBtn = document.getElementById('changeSize');
 const greyscaleBtn = document.getElementById('greyscale');
 const colorfulBtn = document.getElementById('colorful');
 
+// Default grid of 16x16
 let rows = 16;
+// Color mode gets changed based on the buttons pressed.
+let colorMode = '';
 
+// Create the grid. Takes in rows to do the math
 const createPaintingBox = rows => {
   let totalArea = rows * rows;
   container.style.setProperty('grid-template-columns', `repeat(${rows}, auto)`);
@@ -17,60 +21,76 @@ const createPaintingBox = rows => {
   }
 };
 
+// Find all the divs with class .squares.
 const getSquares = () => {
   const squares = document.querySelectorAll('.squares');
   return (allSquares = squares);
 };
 
+/* Add an Event Listener to each box. When you mouse over the box it
+changes the background to a random color. */
 const addColorBox = allSquares => {
   allSquares.forEach(box => {
     box.addEventListener('mouseover', () => {
-      // Set the background to a random color:
       box.style.backgroundColor =
         '#' + (0x1000000 + Math.random() * 0xffffff).toString(16).substr(1, 6);
     });
   });
 };
 
-//Not working yet:
+/* Add an Event Listener to each box. When you mouse over the box it
+changes the background to a shade of grey. From White -> Black in 5 events.*/
 const addGreyscaleBox = allSquares => {
-  let lightness = 100;
   allSquares.forEach(box => {
+    let lightness = 100;
     box.addEventListener('mouseover', () => {
+      if (lightness > 0) {
+        lightness = lightness - 20;
+      }
       box.style.backgroundColor = 'hsl(0, 0%,' + lightness + '%)';
-      lightness = lightness - 10;
     });
   });
 };
 
+// Remove all the divs
 const removePaintingBox = () => {
   while (container.hasChildNodes()) {
     container.removeChild(container.firstChild);
   }
 };
 
+// Change the color mode to colorful
 colorfulBtn.addEventListener('click', () => {
   getSquares();
   addColorBox(allSquares);
+  colorMode = 'colorful';
 });
 
+//Change the color mode to greyscale
 greyscaleBtn.addEventListener('click', () => {
   getSquares();
   addGreyscaleBox(allSquares);
+  colorMode = 'greyscale';
 });
 
+// Change the size of the canvas.
 changeSizeBtn.addEventListener('click', function changeSize() {
   let size = prompt('How many squares across would you like? (1-100)');
-  if (size < 1 || size > 100 || isNaN(size)) {
+  if (size === null || size === '');
+  else if (size < 1 || size > 100 || size % 1 != 0 || isNaN(size)) {
     alert('Invalid Number. Please try again.');
     changeSize();
   } else {
     rows = size;
+    removePaintingBox();
+    createPaintingBox(rows);
+    getSquares();
+    if (colorMode === 'colorful') {
+      addColorBox(allSquares);
+    } else if (colorMode === 'greyscale') {
+      addGreyscaleBox(allSquares);
+    }
   }
-  removePaintingBox();
-  createPaintingBox(rows);
-  getSquares();
-  addColorBox(allSquares);
 });
 
 createPaintingBox(rows);
